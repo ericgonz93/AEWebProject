d118001602ccb5dc7fd0f0210f40ca54436d44f1
ericgonz93 <ericgonz93@hotmail.com>
2016-03-06 16:08:19 -0600

    Testing Testing 125

---
+++ build/reactTest.js
@@ -0,0 +1,12 @@
+//React.Js Attempts
+
+var TestApp = React.createClass({
+
+  render: function () {
+    return React.createElement(
+      "h1",
+      null,
+      "Testing More"
+    );
+  }
+});
---
+++ src/reactTest.js
@@ -0,0 +1,8 @@
+//React.Js Attempts
+
+var TestApp = React.createClass({
+
+render: function(){
+return <h1>Testing More</h1>;
+}
+});
